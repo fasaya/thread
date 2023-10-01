e9532353b7e6455f3fc13e72b652633c3150c6f4
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-10-01 17:16:53 +0800

    add rightsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -21,7 +21,7 @@ function LeftSidebar() {
                             key={link.label}
                             className={`leftsidebar_link ${isActive && 'bg-primary-500'}`} >
                             <Image src={link.imgURL} alt={link.label} width={24} height={24} />
-                            <p className="text-light-1 max-lg:hidden"></p>
+                            <p className="text-light-1 max-lg:hidden">{link.label}</p>
                         </Link>
                     )
                 })}
