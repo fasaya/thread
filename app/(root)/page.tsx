6b57cdd6a1a72f4971e877c3c6213c743224efde
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-10-01 16:52:08 +0800

    add topbar and left sidebar

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+// import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  // return (
+  //     <main>
+  //       <UserButton afterSignOutUrl="/" />
+  //     </main>
+  // )
+
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
+  )
+}
