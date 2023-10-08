c496bb3076f7fdc1e6ac0aba81aa0358d234c9a5
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-10-08 18:50:54 +0800

    update user card component

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,3 +1,8 @@
+'use client'
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+
 interface Props {
     id: string;
     name: string;
@@ -13,8 +18,33 @@ function UserCard({
     imgUrl,
     personType,
 }: Props) {
+
+    const router = useRouter()
+
     return (
-        <div>UserCard</div>
+        <article className="user-card">
+            <div className="user-card_avatar">
+                <Image
+                    src={imgUrl}
+                    alt="logo"
+                    width={48}
+                    height={48}
+                    className="rounded-full"
+                />
+
+                <div className="flex-1 text-ellipsis">
+                    <h4 className="text-0base-semibold text-light-1">{name}</h4>
+                    <p className="text-small-medium text-gray-1">@{username}</p>
+                </div>
+
+                <Button
+                    className="user-card_btn"
+                    onClick={() => router.push(`/profile/${id}`)}>
+                    View
+                </Button>
+
+            </div>
+        </article>
     )
 }
 
